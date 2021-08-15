e9845e5f2575d5f0d16df7d934158a7e5d5b0563
Andrew Njoo <adnjoo@gmail.com>
2021-08-15 13:25:48 -0700

    edit

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -9,9 +9,17 @@ function UserNav ({logout}){
     <Nav className='me-auto'>
       <Nav.Link href={'/popular'}>Popular</Nav.Link>
     </Nav>
-    <Nav>
-      <NavDropdown className="ml-auto" title="Menu" id="basic-nav-dropdown">
-        <NavDropdown.Item href={"/dashboard"}>My Movies</NavDropdown.Item>
+    <Nav
+  
+    >
+      <NavDropdown 
+      align="end"
+      flip
+      title="Menu" 
+      id="basic-nav-dropdown">
+        <NavDropdown.Item 
+        
+        href={"/dashboard"}>My Movies</NavDropdown.Item>
         <NavDropdown.Item href={"/profile"}>My Profile</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item onClick={
@@ -28,7 +36,9 @@ function UserNav ({logout}){
 function GuestNav (props){
   return (
     <Nav>
-      <NavDropdown className="ml-auto" title="Login" id="basic-nav-dropdown">
+      <NavDropdown 
+      alignLeft
+      className="ml-auto" title="Login" id="basic-nav-dropdown">
         <NavDropdown.Item href={"/login"}>Login</NavDropdown.Item>
         <NavDropdown.Item href={"/register"}>Get Started</NavDropdown.Item>
       </NavDropdown>
@@ -55,7 +65,7 @@ const MyNavBar = ({ setAuth, isAuth }) => {
 
   return (
     <div>
-      <Navbar bg="primary" variant="dark">
+      <Navbar bg="primary" variant="dark" >
         <Container>
           <Navbar.Brand href={"/"}>movieApp</Navbar.Brand>
           <LoggedNav isAuth={isAuth} logout={logout}/>
