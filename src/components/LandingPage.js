1fe9f94084dc284838cfd8cff1867e27439c7745
Andrew Njoo <adnjoo@gmail.com>
2021-10-30 11:59:44 +0200

    edit

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -18,13 +18,13 @@ class LandingPage extends Component {
 
   componentDidMount() {
     this.getPopular();
-    document.addEventListener('keydown',this.leftrightdetection, false)
+    document.addEventListener("keydown", this.leftrightdetection, false);
   }
 
   getPopular() {
     let stateCopy = Object.assign({}, this.state);
-    stateCopy.movies = []
-    this.setState(stateCopy)
+    stateCopy.movies = [];
+    this.setState(stateCopy);
     axios
       .get(
         `https://api.themoviedb.org/3/movie/popular?api_key=${tmdbKey}&language=en-US&page=${this.state.counter}`
@@ -36,7 +36,7 @@ class LandingPage extends Component {
           movies: res.data.results,
         };
         this.setState(newState);
-        console.log('state is', this.state);
+        console.log("state is", this.state);
       });
   }
   popularList() {
@@ -46,38 +46,66 @@ class LandingPage extends Component {
   }
   increaseCounter(num) {
     if (this.state.counter === 1 && num === -1) {
-      console.log('not allow')
+      console.log("not allow");
       return;
     }
     let stateCopy = Object.assign({}, this.state);
     stateCopy.counter += num;
     this.setState(stateCopy);
-    setTimeout(()=>{
-      console.log('counter/state', this.state)
-      this.getPopular()
-    },100)
-
+    setTimeout(() => {
+      console.log("counter/state", this.state);
+      this.getPopular();
+    }, 100);
   }
-  leftrightdetection(e){
+  leftrightdetection(e) {
     //left arrow
-    if(e.keyCode==37){ 
-      this.increaseCounter(-1)
+    if (e.keyCode === 37) {
+      this.increaseCounter(-1);
     }
-    if(e.keyCode==39){ 
-      this.increaseCounter(1)
+    if (e.keyCode === 39) {
+      this.increaseCounter(1);
     }
   }
   render() {
     return (
-      <Container className="my-5 w-75 text-center" >
+      <Container className="my-5 w-75 text-center">
         <h3 className="text-center">Popular Movies</h3>
-        <div>page {this.state.counter}
-        </div>
-        <button className='btn btn-info border border-dark' onClick={() => {this.increaseCounter(-1)}}>&lt;</button> &nbsp;
-        <button className='btn btn-info border border-dark' onClick={() => {this.increaseCounter(1)}}>&gt;</button>
-        <div className='text-center my-5'>{this.popularList()}</div>
-        <button className='btn btn-info border border-dark' onClick={() => {this.increaseCounter(-1)}}>&lt;</button> &nbsp;
-        <button className='btn btn-info border border-dark' onClick={() => {this.increaseCounter(1)}}>&gt;</button>
+        <div>page {this.state.counter}</div>
+        <button
+          className="btn btn-info border border-dark"
+          onClick={() => {
+            this.increaseCounter(-1);
+          }}
+        >
+          &lt;
+        </button>{" "}
+        &nbsp;
+        <button
+          className="btn btn-info border border-dark"
+          onClick={() => {
+            this.increaseCounter(1);
+          }}
+        >
+          &gt;
+        </button>
+        <div className="text-center my-5">{this.popularList()}</div>
+        <button
+          className="btn btn-info border border-dark"
+          onClick={() => {
+            this.increaseCounter(-1);
+          }}
+        >
+          &lt;
+        </button>{" "}
+        &nbsp;
+        <button
+          className="btn btn-info border border-dark"
+          onClick={() => {
+            this.increaseCounter(1);
+          }}
+        >
+          &gt;
+        </button>
       </Container>
     );
   }
