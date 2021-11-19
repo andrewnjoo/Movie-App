76e2c7250716da981c14cbb03ebff4e25cb225ac
Andrew Njoo <adnjoo@gmail.com>
2021-11-18 19:27:07 -0800

    add burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,53 +2,49 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import { toast } from "react-toastify";
-const logo = require( '../assets/logo.png')
+const logo = require("../assets/logo.png");
 
-function UserNav ({logout}){
+function UserNav({ logout }) {
   return (
     <>
-    <Nav className='me-auto'>
-      <Nav.Link href={'/movies'}>Movies</Nav.Link>
-      <Nav.Link href={'/television'}>TV Shows</Nav.Link>
-      <Nav.Link href={'/dashboard'}>My Movies</Nav.Link>
-      {/* <Nav.Link href={'/moviesivewatched'}>Movies watched</Nav.Link> */}
-    </Nav>
-    <Nav
-  
-    >
-      <NavDropdown 
-      align="end"
-      flip
-      title="Menu" 
-      id="basic-nav-dropdown">
-        <NavDropdown.Item href={"/profile"}>My Profile</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item onClick={
-          (e)=>{
-            logout(e)
-          }
-          }>Logout</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href={"/movies"}>Movies</Nav.Link>
+        <Nav.Link href={"/television"}>TV Shows</Nav.Link>
+        <Nav.Link href={"/dashboard"}>My Movies</Nav.Link>
+      </Nav>
+      <Nav>
+        <NavDropdown align="end" flip title="Menu" id="basic-nav-dropdown">
+          <NavDropdown.Item href={"/profile"}>My Profile</NavDropdown.Item>
+          <NavDropdown.Item
+            onClick={(e) => {
+              logout(e);
+            }}
+          >
+            Logout
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      </Navbar.Collapse>
     </>
-  )
+  );
 }
 
-function GuestNav (props){
+function GuestNav(props) {
   return (
     <Nav>
-                <Nav.Link href={"/login"}>Login</Nav.Link>
-
+      <Nav.Link href={"/login"}>Login</Nav.Link>
     </Nav>
   );
 }
 
-export const LoggedNav = ({isAuth, logout}) => {
+export const LoggedNav = ({ isAuth, logout }) => {
   // console.log('loggednav isauth', isAuth)
-  if(isAuth){
-    return <UserNav logout={logout}/>
+  if (isAuth) {
+    return <UserNav logout={logout} />;
   } else {
-    return <GuestNav/>
+    return <GuestNav />;
   }
 };
 
@@ -63,19 +59,19 @@ const NavBar = ({ setAuth, isAuth }) => {
 
   return (
     <div>
-      <Navbar className='primarycolor' variant="dark" >
+      <Navbar className="primarycolor" variant="dark" expand='lg'>
         <Container>
           <Navbar.Brand href={"/"}>
-          <img
-          alt=""
-          src={logo.default}
-          width="30"
-          height="30"
-          className="d-inline-block align-top rounded-circle"
-        />{' '}
+            <img
+              alt=""
+              src={logo.default}
+              width="30"
+              height="30"
+              className="d-inline-block align-top rounded-circle"
+            />{" "}
             movieApp
-            </Navbar.Brand>
-          <LoggedNav isAuth={isAuth} logout={logout}/>
+          </Navbar.Brand>
+          <LoggedNav isAuth={isAuth} logout={logout} />
         </Container>
       </Navbar>
     </div>
