08b144763e5b4a8995fad98db663b61de4694c8b
Andrew Njoo <adnjoo@gmail.com>
2022-01-31 23:34:17 -0800

    add eslint

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
 // import { Link } from "react-router-dom";
-import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
-import { toast } from "react-toastify";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
-const logo = require("../assets/logo.png");
+import {
+  Nav, Navbar, Container, NavDropdown,
+} from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+const logo = require('../assets/logo.png');
 
 function UserNav({ logout }) {
   return (
@@ -12,9 +15,9 @@ function UserNav({ logout }) {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href={"/movies"}>Movies</Nav.Link>
-          <Nav.Link href={"/television"}>TV Shows</Nav.Link>
-          <Nav.Link href={"/dashboard"}>My Movies</Nav.Link>
+          <Nav.Link href="/movies">Movies</Nav.Link>
+          <Nav.Link href="/television">TV Shows</Nav.Link>
+          <Nav.Link href="/dashboard">My Movies</Nav.Link>
         </Nav>
         <Nav>
           <NavDropdown
@@ -22,9 +25,9 @@ function UserNav({ logout }) {
             flip
             title={<FontAwesomeIcon icon={faUserCircle} size="2x" />}
             id="basic-nav-dropdown"
-            style={{ paddingRight: "20px" }}
+            style={{ paddingRight: '20px' }}
           >
-            <NavDropdown.Item href={"/profile"}>My Profile</NavDropdown.Item>
+            <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
             <NavDropdown.Item
               onClick={(e) => {
                 logout(e);
@@ -39,51 +42,51 @@ function UserNav({ logout }) {
   );
 }
 
-function GuestNav(props) {
+function GuestNav() {
   return (
     <Nav>
-      <Nav.Link href={"/login"} style={{ paddingRight: "20px" }}>
+      <Nav.Link href="/login" style={{ paddingRight: '20px' }}>
         Login
       </Nav.Link>
     </Nav>
   );
 }
 
-export const LoggedNav = ({ isAuth, logout }) => {
+export function LoggedNav({ isAuth, logout }) {
   // console.log('loggednav isauth', isAuth)
   if (isAuth) {
     return <UserNav logout={logout} />;
-  } else {
-    return <GuestNav />;
   }
-};
+  return <GuestNav />;
+}
 
-const NavBar = ({ setAuth, isAuth }) => {
-  //logout function
+function NavBar({ setAuth, isAuth }) {
+  // logout function
   const logout = (e) => {
     e.preventDefault();
-    localStorage.removeItem("token");
+    localStorage.removeItem('token');
     setAuth(false);
-    toast.success("Logged out successfully.");
+    toast.success('Logged out successfully.');
   };
 
   return (
     <Navbar className="primarycolor" variant="dark" expand="md" id="top-header">
       <Container>
-        <Navbar.Brand href={"/"} style={{ paddingLeft: "20px" }}>
+        <Navbar.Brand href="/" style={{ paddingLeft: '20px' }}>
           <img
             alt=""
             src={logo.default}
             width="30"
             height="30"
             className="d-inline-block align-top rounded-circle"
-          />{" "}
+          />
+          {' '}
           Movie App
         </Navbar.Brand>
         <LoggedNav isAuth={isAuth} logout={logout} />
       </Container>
     </Navbar>
   );
-};
+}
 
 export default NavBar;
