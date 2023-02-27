b98de558d6aeeea41e5d688555ac5d734000027f
Andrew Njoo <adnjoo@gmail.com>
2023-02-27 11:24:23 -0800

    style: remove decimal places from rating

--- old-frontend/src/components/ui/CustomCircularProgressBar.tsx
+++ old-frontend/src/components/ui/CustomCircularProgressBar.tsx
@@ -3,16 +3,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BsPercent } from 'react-icons/bs';
 import { IconContext } from 'react-icons';
-import { buildStyles, CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import {
+  buildStyles,
+  CircularProgressbarWithChildren,
+} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 function CustomCircularProgressBar({
-  data, transformData = 'translate(8px, -10px)', width = 'w-8', textSize = 'text-xs',
+  data,
+  transformData = 'translate(8px, -10px)',
+  width = 'w-8',
+  textSize = 'text-xs',
 }) {
+  const voteAverage = (data.vote_average * 10).toFixed(0);
   return (
     <div className={width} style={{ transform: transformData }}>
       <CircularProgressbarWithChildren
-        value={data.vote_average * 10}
+        value={String(voteAverage)}
         background
         styles={buildStyles({
           pathColor: data.vote_average > 7 ? '#39d07a' : '#d2d531',
@@ -21,8 +28,13 @@ function CustomCircularProgressBar({
         })}
       >
         <span className={`flex align-center text-white ${textSize}`}>
-          {data.vote_average * 10}
-          <IconContext.Provider value={{ size: '0.6em', style: { transform: 'translate(0px, 0.3em)' } }}>
+          {voteAverage}
+          <IconContext.Provider
+            value={{
+              size: '0.6em',
+              style: { transform: 'translate(0px, 0.3em)' },
+            }}
+          >
             <BsPercent />
           </IconContext.Provider>
         </span>
