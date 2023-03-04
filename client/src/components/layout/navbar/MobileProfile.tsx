b2f28c3218487c7f91c171df4e9a8f11fedae413
Andrew Njoo <adnjoo@gmail.com>
2023-03-04 04:38:20 -0800

    style: remove placeholder text from MobileProfile

--- client/src/components/layout/navbar/MobileProfile.tsx
+++ client/src/components/layout/navbar/MobileProfile.tsx
@@ -16,25 +16,13 @@ export const MobileProfile = ({
     <>
       {isAuthorized ? (
         <div className='pt-4 pb-3 border-t border-gray-200'>
-          <div className='flex items-center px-4'>
-            <div className='flex-shrink-0'>
-              <FaUserCircle className='h-8 w-8' />
-            </div>
-            <div className='ml-3'>
-              <div className='text-base font-medium text-gray-800'>
-                Tom Cook
-              </div>
-              <div className='text-sm font-medium text-gray-500'>
-                tom@example.com
-              </div>
-            </div>
-          </div>
           <div className='mt-3 space-y-1'>
             <Disclosure.Button
               as='a'
               href='#'
-              className='block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100'
+              className='flex gap-2 items-center px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100'
             >
+              <FaUserCircle className='h-8 w-8' />
               Your Profile
             </Disclosure.Button>
             <button
