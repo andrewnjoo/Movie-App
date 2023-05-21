13a73f43b7b79adffb49a8d64f48452849b4bad2
Andrew Njoo <adnjoo@gmail.com>
2023-05-21 09:33:35 -0700

    fix: add @ path to tsconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
 });
