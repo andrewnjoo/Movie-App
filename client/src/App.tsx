f2ca2d13709c1bc2168e72f8a3af3a2be23f5d43
Andrew Njoo <adnjoo@gmail.com>
2023-05-29 08:07:49 -0700

    fix: try 8 revert to MoviePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,8 +22,8 @@ function App(): JSX.Element {
           <Route index element={<Home />} />
           <Route path="/movies" element={<Home />} />
           <Route path="/movies/:id" element={<MoviePage />} />
-          {/* <Route path="/tv" element={<Home movie={false} />} /> */}
-          <Route path="/tv" element={<TestPage />} />
+          <Route path="/tv" element={<Home movie={false} />} />
+          {/* <Route path="/tv" element={<TestPage />} /> */}
           <Route path="/tv/:id" element={<MoviePage movie={false} />} />
           <Route path="/people" element={<PeoplePage />} />
           <Route path="/people/:id" element={<PersonPage />} />
