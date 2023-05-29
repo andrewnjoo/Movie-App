0f9b099872ab2a8a74255936a83e1273c282597e
Andrew Njoo <adnjoo@gmail.com>
2023-05-29 08:45:43 -0700

    fix: try 10, try API v4

--- client/src/config/tmdbConfig.ts
+++ client/src/config/tmdbConfig.ts
@@ -1,4 +1,4 @@
-const tmdbApi = 'https://api.themoviedb.org/3';
+const tmdbApi = 'https://api.themoviedb.org/4';
 const tmdbKey = import.meta.env.VITE_TMDB_KEY;
 
 export { tmdbApi, tmdbKey };
--- client/src/redux/tv.ts
+++ client/src/redux/tv.ts
@@ -4,11 +4,9 @@ import type { Tv } from '#/types';
 import { tmdbApi, tmdbKey } from '@/config';
 import type { GetTVReq, GetTVRes } from '@/types';
 
-const baseUrl = 'https://api.themoviedb.org/3';
-
 export const tvApi = createApi({
   reducerPath: 'tmdbTvApi',
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: fetchBaseQuery({ baseUrl: tmdbApi }),
   endpoints: (builder) => ({
     getTV: builder.query<GetTVRes, GetTVReq>({
       query: (page = 1) =>
